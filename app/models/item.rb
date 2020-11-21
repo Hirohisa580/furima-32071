0c71fdbc8818a975eafb4dcb22e858b7f71d68de
Hirohisa580 <koyahiro13@gmail.com>
2020-11-22 00:19:33 +0900

    商品出品機能の実装とモデルの単体テストコード記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,30 @@
 class Item < ApplicationRecord
+  
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping
+  belongs_to :area
+  belongs_to :day
+
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_id
+    validates :area_id
+    validates :days_id
+  end
+
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :explanation
+    validates :image
+  end
+
+  validates :price, format: {with: /\A[0-9]+\z/}, 
+                    numericality: { :greater_than_or_equal_to  => 300, :less_than_or_equal_to => 9999999  }
 end
